a1a47ba7f1a73efec1ef9e8c1e61c551b9fe7bd1
tracie wamsley <traciewamsley@gmail.com>
2015-07-29 10:15:51 -0600

    refractored server and added to controllers

--- controllers/story_controller.js
+++ controllers/story_controller.js
@@ -1,2 +1,10 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+exports.index = function(req, res) {
+    res.render('story');
+};
+
+exports.create = function(req, res) {
+    res.render('createStory');
+};
