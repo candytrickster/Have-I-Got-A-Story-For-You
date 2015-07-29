8b2552ec2fad3706d23a4de813e41d85316f44ee
tracie wamsley <traciewamsley@gmail.com>
2015-07-29 08:55:06 -0600

    very basic server

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    jade = require('jade'),
+    path = require('path');
+
+var app = express(); 
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public'))); 
+
+//home
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+//login screen
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+//sign up screen
+app.get('/signup', function (req, res) {
+    res.render('signup');
+});
+
+//view a clicked story
+app.get('/story', function (req, res) {
+    res.render('story');
+});
+
+//create a new story
+app.get('/create', function (req, res) {
+    res.render('create');
+});
+
+
+app.listen(3000);
