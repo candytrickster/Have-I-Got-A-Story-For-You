f82517438a2519ddfe4c10a8b750e00af407f817
Kailtynn <kailtynnkruse@gmail.com>
2015-07-31 08:18:44 -0600

    [WIP]Authorization start

--- server.js
+++ server.js
@@ -12,14 +12,22 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public'))); 
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+<<<<<<< Updated upstream
 app.get('/', story.home); //app.get('/', index.home);
 app.get('/login', user.login);
 app.get('/register', user.register);
 app.get('/story', story.index);
 app.get('/create', story.create);
 app.post('/create', urlencodedParser, story.add_story);
+=======
+app.get('/', index.renderHome);
+app.get('/login', user.renderLogin);
+app.get('/register', user.renderRegister);
+app.post('/register', user.register);
+app.get('/story', story.renderIndex);
+app.get('/create', story.renderCreate);
+>>>>>>> Stashed changes
 
 app.listen(3000);
